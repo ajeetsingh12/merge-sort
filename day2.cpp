e4ccb9cc7d371608cecd99ace8f1d746b6a7d4db
ajeetsingh12 <62153367+ajeetsingh12@users.noreply.github.com>
2020-10-06 18:50:35 +0530

    Add files via upload
    added a cpp file

---
+++ day2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main(){
+    system("color e6");
+    cout<<"enter amy text then enter then you will the changes is background of cmd";
+    int a;
+    cin>>a;
+    system("color a6");
+    return 0;
+}
